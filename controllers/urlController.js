891b66c10cbfba7ee7a898866fefbfbe61458e6f
Sandeep Upadhyay <157040731+sandeep-up@users.noreply.github.com>
2026-05-22 14:04:01 +0530

    Nothing special

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,19 +1,44 @@
 const url=require("../models/urlModel");
-const createShortURL=function(req,res){
-      const shortId=Math.random().toString(36).substring(2,8);
+const createShortURL= async function(req,res){
+     const originalURL = req.body.url;
+
+    // 1. Check if URL already exists
+    const existing = await url.findOne({ redirectURL: originalURL });
+
+    if(existing){
+        const allUrls = await url.find();
+
+        return res.render("home", {
+            shortUrl: "http://localhost:3000/" + existing.shortId,
+            urls: allUrls
+        });
+    }
+      // Generate unique shortId
+    let shortId;
+    let isUnique = false;
+
+    while(!isUnique){
+        shortId = Math.random().toString(36).substring(2,8);
+
+        const existingId = await url.findOne({ shortId: shortId });
+
+        if(!existingId){
+            isUnique = true;
+        }
+    }
       const newURL= new url({
           shortId:shortId,
-          redirectURL:req.body.url
+          redirectURL:originalURL
       });
-      newURL.save().then(function(data){
-             res.json(
-                "http://localhost:3000/"+shortId
-             );
-      });
+      await newURL.save();
+      const allUrls=await url.find();
+    res.render("home", {
+    shortUrl: "http://localhost:3000/" + shortId,
+    urls:allUrls
+});
 };
 function redirectURL(req,res){
     const shortId = req.params.shortId;
-
     url.findOne({ shortId: shortId }).then(function(entry){
         if(!entry){
             return res.send("URL not found");
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require("express");
 const app=express();
+app.set("view engine","ejs");
+app.set("views","./views");
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 require("./connection");
 const urlRoutes=require("./routes/urlRoutes");
+const url = require("./models/urlModel");
 app.use("/",urlRoutes);
-app.get("/",function(req,res){
-    res.send("this is the homepage");
-})
+app.get("/", async function (req, res) {
+    const allUrls=await url.find();
+    res.render("home", {
+         shortUrl: null,
+         urls:allUrls
+        });
+});
 app.listen(3000,()=>{
     console.log("server is running at port:3000");
 })
